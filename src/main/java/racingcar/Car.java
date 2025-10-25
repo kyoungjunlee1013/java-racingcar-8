8c7b4567103866970d06f207ec025ee4504ab766
이경준 <kyoungjunlee@igyeongjun-ui-MacBookPro.local>
2025-10-25 23:54:53 +0900

    feat: 자동차별 4 이상일 경우 전진 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -11,6 +11,11 @@ public class Car {
     }
 
     public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+        if(randomNumber >= 4){
+            this.position++;
+        }
     }
     public String getName() {
         return name;
