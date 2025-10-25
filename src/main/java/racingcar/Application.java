a4f7f61cdb55cfd2591de567071c95519b628047
이경준 <kyoungjunlee@igyeongjun-ui-MacBookPro.local>
2025-10-25 16:57:21 +0900

    refactored: run 함수 무게 줄이기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,15 +10,31 @@ public class Application {
         run();
     }
     public static void run(){
+        List<Car> cars = setupCars();
+
+
+    }
+    private static List<Car> setupCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String line = Console.readLine();
+        String input = Console.readLine();
 
-        String[] names = line.split(",");
+        String[] carName = input.split(",");
 
-        List<Car> cars = new ArrayList<>();
-        for (String name : names) {
-            cars.add(new Car(name.trim()));
+        List <Car> cars = new ArrayList<>();
+
+        for(String name : carName){
+            String trimmedName = name.trim();
+            validateCarName(trimmedName);
+            cars.add(new Car(trimmedName));
         }
 
+        return cars;
+    }
+    private static String validateCarName(String name){
+        if(name.isEmpty() || name.length() >5){
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+        }
+
+        return name;
     }
 }
