69024ae547ef7257dc333a59d1b0306abf64a6df
이경준 <kyoungjunlee@igyeongjun-ui-MacBookPro.local>
2025-10-25 16:34:50 +0900

    feat:경주할 자동차 이름 쉼표로 구분하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        run();
+    }
+    public static void run(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String line = Console.readLine();
+
+        String[] names = line.split(",");
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name.trim()));
+        }
+
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int position;
+
+    Car(String name) {
+        this.name = name;
+    }
+
+}
